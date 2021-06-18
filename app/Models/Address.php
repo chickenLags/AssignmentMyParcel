db5f09d734d3b57d432de63e791966a30bef5c07
Roy Schouten <royschouten@sterrkdeta.nl>
2021-06-18 11:16:03 +0200

    shipment and address models created

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "street_name",
+        "street_number",
+        "country_code",
+        "first_name",
+        "last_name",
+        "phone",
+    ];
+
+    public function getAddress() {
+        return "$this->street_name $this->street_number";
+    }
+
+    public function getFullName() {
+        return "$this->first_name $this->last_name";
+    }
 }
--- app/Models/Shipment.php
+++ app/Models/Shipment.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Shipment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'number_of_items',
+        'service_code',
+        'recipient_address',
+        'tracking_code',
+        'deliver_at'
+    ];
+
+    public function recipientAddress() {
+        return $this->hasOne(Address::class);
+    }
 }
