f9f0327b577422cc0f7933baff5fe057d2e48d51
Roy Schouten <royschouten@sterrkdeta.nl>
2021-06-18 14:41:32 +0200

    Polished Feature ShipmentToConsignmentTest and created Unittest for validation

--- tests/Feature/ShipmentToConsignmentTest.php
+++ tests/Feature/ShipmentToConsignmentTest.php
@@ -11,7 +11,6 @@ use Tests\TestCase;
 
 class ShipmentToConsignmentTest extends TestCase
 {
-
     protected array $jsonRequest;
     protected array $expectedJson;
     protected array $container = [];
@@ -59,6 +58,7 @@ class ShipmentToConsignmentTest extends TestCase
 
     /**
      * Feature test of Shipping to Consignment.
+     * asserts a response 200 response and that the expected json is returned.
      *
      * @return void
      */
@@ -69,11 +69,35 @@ class ShipmentToConsignmentTest extends TestCase
             ->assertJson($this->expectedJson);
     }
 
+    /**
+     * Tests that shipping endpoint uses the customValidator.
+     * asserts a response 422 response when no json/body is sent.
+     *
+     * @return void
+     */
+    public function test_shipmentsFeatureUsesStoreshipmentrequestTest()
+    {
+        $this->json('POST', '/api/shipments')
+            ->assertStatus(422);
+    }
+
+    /**
+     * Tests that Guzzle Client has indeed made one call.
+     * In the ShipmentController@store there is exactly one call made with Guzzle. This test asserts that.
+     *
+     * @return void
+     */
     public function test_ReachedOutToRemoteServerUnitTest() {
         $this->json('POST', '/api/shipments', $this->jsonRequest);
         $this->assertTrue(count($this->container) === 1, 'Guzzle client/ foreign server was not called in the test.');
     }
 
+    /**
+     * Tests that Guzzle reached out to the correct endpoint.
+     * Asserts that the route foreign-server.com/consigment was called.
+     *
+     * @return void
+     */
     public function test_CorrectRemoteCarrierHostAndPathUnitTest(){
         $this->json('POST', '/api/shipments', $this->jsonRequest);
         $guzzleUri = $this->container[0]['request']->getUri();
